a073eb761909067130dece20a55d8d93e159035b
DunnoDesign <dunnoagency@gmail.com>
2024-02-03 18:44:51 -0300

    mongodb atlas

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,10 +1,10 @@
-const TOKEN_SECRET ='secreto de adriel'
+const TOKEN_SECRET = process.env.TOKEN_SECRET || 'secreto de adriel'
 
-const FRONTEND_URL = 'http://localhost:5173'
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
 
-const DB_HOST = "mongodb://localhost/shorturl"
+const DB_HOST = process.env.DB_HOST || "mongodb+srv://adrielmachado:GBNXMj3kd932Q0I9@shorturl.kv9vlnl.mongodb.net/"
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 module.exports = {
     TOKEN_SECRET,
