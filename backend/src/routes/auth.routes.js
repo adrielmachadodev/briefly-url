84c5e34bb19a8dc20bcd234276890571140275a4
DunnoDesign <dunnoagency@gmail.com>
2024-02-05 00:35:25 -0300

    logout de backend out

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,12 +2,10 @@ const { Router } = require('express')
 
 const register = require('../controllers/register.controllers.js')
 const login = require('../controllers/login.controllers.js')
-const logout = require('../controllers/logout.controllers.js')
 
 const router = Router()
 
 router.post('/register', register)
 router.post('/login', login)
-router.post('/logout', logout)
 
 module.exports = router
